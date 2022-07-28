258308c00f042915fdb9acfc9044460fb979692d
Amy Suo <67298442+luminarchy@users.noreply.github.com>
2022-07-28 01:14:41 -0400

    Create topdown.py

---
+++ topdown.py
@@ -0,0 +1,36 @@
+numberShuffle = 0
+numberCard = 0
+cardCheck = 52
+deck = []
+deckCopy = []
+while numberCard != cardCheck:
+    numberCard = numberCard + 1
+    deck.append(numberCard)
+    deckCopy.append(numberCard)
+
+print(deck)
+def shuffleCards (deck):
+    midDeck = int(len(deck) / 2)
+    halfDeck = int(midDeck-1)
+    deckA = deck[0:midDeck]
+    deckB = deck[midDeck:len(deck)]
+    deck = []
+    while deckB != []:
+        deck.append(deckA[halfDeck])
+
+        del deckA[halfDeck]
+        deck.append(deckB[halfDeck])
+        del deckB[halfDeck]
+        halfDeck = halfDeck - 1
+    deck.reverse()
+    return deck
+
+deck=shuffleCards (deck)
+numberShuffle = numberShuffle+1
+
+print(deck)
+while (deck != deckCopy):
+    deck=shuffleCards (deck)
+    numberShuffle = numberShuffle+1
+    print(deck)
+print(numberShuffle)
