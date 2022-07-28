1f1fe143d29c1b1761c256403238cc66e026420d
Amy Suo <67298442+luminarchy@users.noreply.github.com>
2022-07-28 01:16:06 -0400

    Create bottomup.py

---
+++ bottomup.py
@@ -0,0 +1,34 @@
+numberShuffle = 0
+numberCard = 0
+cardCheck = 6
+deck = []
+deckCopy = []
+while numberCard != cardCheck:
+    numberCard = numberCard + 1
+    deck.append(numberCard)
+    deckCopy.append(numberCard)
+
+print(deck)
+def shuffleCards (deck):
+    midDeck = int(len(deck) / 2)
+    deckB = deck[0:midDeck]
+    deckA = deck[midDeck:len(deck)]
+    deck = []
+    while deckB != []:
+        deck.append(deckA[0])
+
+        del deckA[0]
+        deck.append(deckB[0])
+        del deckB[0]
+    deck.reverse()
+    return deck
+
+deck=shuffleCards (deck)
+numberShuffle = numberShuffle+1
+
+print(deck)
+while (deck != deckCopy):
+    deck=shuffleCards (deck)
+    numberShuffle = numberShuffle+1
+    print(deck)
+print(numberShuffle)
